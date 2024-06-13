63526c596e774bbd5aeb604ccf63f8b388842af7
David Soane <david.soane@outlook.com>
2024-06-12 20:56:20 -0600

    changed cdebootstrap to install packages

--- create-targz.sh
+++ create-targz.sh
@@ -6,20 +6,27 @@ sudo apt-get -y -q update
 sudo apt-get -y -q install curl gnupg cdebootstrap debian-archive-keyring tar
 
 #create target directory
-sudo mkdir debian-installer
+sudo mkdir -p ~/debian-installer
+cd ~/debian-installer
 
 # create a bootstrap of ISO
-sudo cdebootstrap --arch amd64 bookworm /debian-installer http://deb.debian.org/debian
+sudo cdebootstrap --arch amd64 bookworm --include=vim,git,ssh,man ~/debian-installer/rootfs http://deb.debian.org/debian
 
 # installs necessary packages in directory
-sudo chroot /debian-installer /bin/bash
+sudo chroot ~/debian-installer/rootfs /bin/bash
 
 apt-get update
-apt-get install -y wget vim,sudo,git,ssh,gnupg,man,curl
+# apt-get install -y wget git
+# # apt-get install -y wget vim,sudo,git,ssh,gnupg,man,curl
+
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
 
 exit 
 
 # create tar.gz
-tar -czf deb-installer.tar.gz -C /debian-installer
+sudo tar -czf debian-installer.tar.gz -C ~/debian-installer/rootfs .
 
 echo "ISO image successfully bootstrapped"
